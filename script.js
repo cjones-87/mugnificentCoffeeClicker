02d171b1a84807653f8387c074678c3441bcf696
CJ <cliftonjones.87@gmail.com>
2023-12-27 22:54:27 -0500

    refactor: converted all named functions to arrow functions for readability

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
 const resetButton = document.getElementById('startOver');
-resetButton.addEventListener("click", () => {
-  localStorage.removeItem("gameState");
+resetButton.addEventListener('click', () => {
+  localStorage.removeItem('gameState');
   location.reload();
 });
 
-function updateCoffeeView(coffeeQty) {
-  const coffeeCount = document.getElementById("coffeeCounter");
+const updateCoffeeView = (coffeeQty) => {
+  const coffeeCount = document.getElementById('coffeeCounter');
   coffeeCount.innerText = coffeeQty;
-}
+};
 
-function clickCoffee(data) {
+const clickCoffee = (data) => {
   data.coffee++;
   updateCoffeeView(data.coffee);
   renderProducers(data);
-}
+};
 
-function unlockProducers(producers, coffeeCount) {
+const unlockProducers = (producers, coffeeCount) => {
   producers.forEach((producer) => {
     if (coffeeCount >= producer.price / 2) {
       producer.unlocked = true;
     }
   });
-}
+};
 
-function getUnlockedProducers(data) {
+const getUnlockedProducers = (data) => {
   const unlockedProducers = data.producers.filter(
     (producer) => producer.unlocked
   );
 
   return unlockedProducers;
-}
+};
 
-function makeDisplayNameFromId(id) {
-  return id
+const makeDisplayNameFromId = (id) =>
+  id
     .split(/_/g)
     .map((word) => word[0].toUpperCase() + word.slice(1))
     .join(' ');
-}
 
-function makeProducerDiv(producer) {
+const makeProducerDiv = (producer) => {
   const containerDiv = document.createElement('div');
   containerDiv.className = 'producer';
   const displayName = makeDisplayNameFromId(producer.id);
@@ -56,15 +55,15 @@ function makeProducerDiv(producer) {
       `;
   containerDiv.innerHTML = html;
   return containerDiv;
-}
+};
 
-function deleteAllChildNodes(parent) {
+const deleteAllChildNodes = (parent) => {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
   }
-}
+};
 
-function renderProducers(data) {
+const renderProducers = (data) => {
   const producerContainer = document.getElementById('producerContainer');
 
   unlockProducers(data.producers, data.coffee);
@@ -74,25 +73,21 @@ function renderProducers(data) {
   getUnlockedProducers(data).forEach((producer) =>
     producerContainer.appendChild(makeProducerDiv(producer))
   );
-}
+};
 
-function getProducerById(data, producerId) {
-  return data.producers.find((producer) => producer.id === producerId);
-}
+const getProducerById = (data, producerId) =>
+  data.producers.find((producer) => producer.id === producerId);
 
-function canAffordProducer(data, producerId) {
-  return data.coffee >= getProducerById(data, producerId).price ? true : false;
-}
+const canAffordProducer = (data, producerId) =>
+  data.coffee >= getProducerById(data, producerId).price;
 
-function updateCPSView(cps) {
-  document.getElementById("cps").innerText = cps;
-}
+const updateCPSView = (cps) => (document.getElementById('cps').innerText = cps);
 
-function updatePrice(oldPrice) {
+const updatePrice = (oldPrice) => {
   return Math.floor(oldPrice * 1.11);
-}
+};
 
-function attemptToBuyProducer(data, producerId) {
+const attemptToBuyProducer = (data, producerId) => {
   if (canAffordProducer(data, producerId)) {
     const producer = getProducerById(data, producerId);
     producer.qty++;
@@ -105,49 +100,49 @@ function attemptToBuyProducer(data, producerId) {
   } else {
     return false;
   }
-}
+};
 
-function buyButtonClick(event, data) {
-  if (event.target.tagName === "BUTTON") {
+const buyButtonClick = (event, data) => {
+  if (event.target.tagName === 'BUTTON') {
     const producer = event.target.id.slice(4);
     const trueOrFalse = attemptToBuyProducer(data, producer);
 
     if (!trueOrFalse) {
-      window.alert("Not enough coffee!");
+      window.alert('Not enough coffee!');
     } else {
       renderProducers(data);
       updateCoffeeView(data.coffee);
       updateCPSView(data.totalCPS);
     }
   }
-}
+};
 
 const saveGameState = (data) =>
-  localStorage.setItem("gameState", JSON.stringify(data));
+  localStorage.setItem('gameState', JSON.stringify(data));
 
 const initializeGameState = () => {
-  const savedGameState = localStorage.getItem("gameState");
+  const savedGameState = localStorage.getItem('gameState');
   return savedGameState ? JSON.parse(savedGameState) : getDefaultGameState();
 };
 
 const getDefaultGameState = () => window.data;
 
-function tick(data) {
+const tick = (data) => {
   data.coffee += data.totalCPS;
   updateCoffeeView(data.coffee);
   renderProducers(data);
 
   saveGameState(data);
-}
+};
 
-if (typeof process === "undefined") {
+if (typeof process === 'undefined') {
   const data = initializeGameState();
 
-  const bigCoffee = document.getElementById("bigCoffee");
-  bigCoffee.addEventListener("click", () => clickCoffee(data));
+  const bigCoffee = document.getElementById('bigCoffee');
+  bigCoffee.addEventListener('click', () => clickCoffee(data));
 
-  const producerContainer = document.getElementById("producerContainer");
-  producerContainer.addEventListener("click", (event) => {
+  const producerContainer = document.getElementById('producerContainer');
+  producerContainer.addEventListener('click', (event) => {
     buyButtonClick(event, data);
   });
 
